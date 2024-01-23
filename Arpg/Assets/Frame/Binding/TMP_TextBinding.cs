2904b112ae040c09d9664d8ed2820f05d9d45760
wwc <1469359779@qq.com>
2024-01-23 23:52:47 +0800

    add tmp_text binding

--- Arpg/Assets/Frame/Binding/TMP_TextBinding.cs
+++ Arpg/Assets/Frame/Binding/TMP_TextBinding.cs
@@ -7,5 +7,15 @@ namespace Frame.Binding
     public class TMP_TextBinding:BindingBase
     {
         private TMP_Text _text;
+
+        public override void HandleProxy()
+        {
+            if (_text==null)
+            {
+                _text = GetComponent<TMP_Text>();
+            }
+            _text.text= (_BindingGroup.HandleProxy(realPath) as FieldProxy<string>)?.value;
+            Debug.Log(_text.text);
+        }
     }
 }
